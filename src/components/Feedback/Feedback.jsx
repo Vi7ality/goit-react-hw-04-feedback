24b41ff4b6a0ba62c00b3d274e5015b36aeec5e0
Vitalii <svitalii138@gmail.com>
2022-12-17 12:54:28 +0200

    Total count added

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,47 +1,99 @@
-import PropTypes from 'prop-types'
-import React, { Component } from "react";
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
-
-
-    
-class Feedback extends Component {
-    static defaultProps = {
-      
+export class Feedback extends Component {
+  static defaultProps = {
+    step: 1,
   };
 
-    static propTypes = {};
-    
-    state = {
-  good: 0,
-  neutral: 0,
-  bad: 0
-}
+  static propTypes = {};
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleIncrement = value => {
+    this.setState(prevState => ({
+      [value]: prevState[value] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    Object.values(this.state).reduce((acc, value) => {
+      
+        return acc + value
+      }, 0)
+
+  };
 
   render() {
-    return     <div>
-<h1>Please, leave feedback</h1>
-      <ul>
-        <li><button type="button">Good</button></li>
-        <li><button type="button">Neutral</button></li>
-        <li><button type="button">Bad</button></li>
-      </ul>
+    const { good, neutral, bad } = this.state;
+
+
+    return (
       <div>
-        <h2>Statistics</h2>
+        <h1>Please, leave feedback</h1>
         <ul>
           <li>
-            <p>Good:</p>
-            <p></p>
+            <button
+              onClick={() => {
+                this.handleIncrement('good');
+              }}
+              type="button"
+            >
+              Good
+            </button>
           </li>
           <li>
-            <p>Neutral:</p>
-            <p></p>
+            <button
+              onClick={() => {
+                this.handleIncrement('neutral');
+              }}
+              type="button"
+            >
+              Neutral
+            </button>
           </li>
           <li>
-            <p>Bad:</p>
-            <p></p>
+            <button
+              onClick={() => {
+                this.handleIncrement('bad');
+              }}
+              type="button"
+            >
+              Bad
+            </button>
           </li>
         </ul>
+        <div>
+          <h2>Statistics</h2>
+          <ul>
+            <li>
+              <p>Good:</p>
+              <p>{good}</p>
+            </li>
+            <li>
+              <p>Neutral:</p>
+              <p>{neutral}</p>
+            </li>
+            <li>
+              <p>Bad:</p>
+              <p>{bad}</p>
+            </li>
+
+            <li>
+              <p>Total:</p>
+              <p>{this.countTotalFeedback}</p>
+            </li>
+            <li>
+              <p>Positive feedback:</p>
+              <p></p>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>;
+    );
   }
 }
